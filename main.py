8657543ce00f42db9bb1358bf27178f4c84e8869
linzj <manjian2006@gmail.com>
2014-08-25 10:38:26 +0800

    wait for 120s is a good choice.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
         PageRoutines.do_main_page(PageDelegate.HttpRequest('www.douban.com', '/group/', jar = cj), {'topic' : 'td.td-subject>a.title', 'next_page' : 'span.next>a', 'url_attrib' : 'href'}, main_page_delegate)
         while HttpFetchProcess.next():
             pass
-        time.sleep(5)
+        time.sleep(120)
 
 if __name__ == '__main__':
     main()
