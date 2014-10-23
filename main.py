f74da13324d48cddeb4a9f83a8440de544bf5f79
linzj <linzj@ucweb.com>
2014-10-23 14:55:57 +0800

    * main.py: Use configure file to do the download.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import Page, PageDelegate, HttpFetchProcess
-import cookielib, time
+import cookielib, time, sys
 from Print import printDebug
 from ImageSaver import flush_log
 
@@ -24,32 +24,37 @@ class SleepForClass(object):
             printDebug('begining next pass')
             self.count_ = 0
 
-MAX_PARALLEL_PAGES=10
+
+def get_args_read():
+    if len(sys.argv) != 2:
+        printError("need to specify map file")
+        sys.exit(1);
+    with open(sys.argv[1], 'r') as f:
+        content = f.read()
+
+    return eval(content)
 
 
 def main():
+    configure = get_args_read()
+    if not 'host' in configure:
+        printError('There should be "host" field in your map')
+        sys.exit(1)
+    if not 'path' in configure:
+        printError('There should be "path" field in your map')
+        sys.exit(1)
+    if 'max_parallel_pages' in configure:
+        MAX_PARALLEL_PAGES = configure['max_parallel_pages']
+    else:
+        MAX_PARALLEL_PAGES = 1
     cj = cookielib.MozillaCookieJar()
     cj.load('./cookies.txt')
     HttpFetchProcess.start()
-    #main_dict = {
-    #    'target' : 'td.td-subject>a.title',
-    #    'next_page' : 'span.next>a',
-    #    'url_attrib' : 'href',
-    #    'sub' : {
-    #        'target' : 'div.topic-figure.cc>img',
-    #        'next_page' : '',
-    #        'url_attrib' : 'src'},
-    #}
-    main_dict = {
-        'target' : 'div.row>div.text>p>img',
-        'next_page' : 'a.previous-comment-page',
-        'url_attrib' : ['org_src', 'src'],
-    }
-    main_request = PageDelegate.HttpRequest('jandan.net', '/ooxx/', jar = cj)
+    main_dict = configure
+    main_request = PageDelegate.HttpRequest(configure['host'], configure['path'], jar = cj)
     new_page_requests = []
     sleeper = SleepForClass()
     while True:
-
         for i in range(MAX_PARALLEL_PAGES):
             main_page_delegate = PageDelegate.PageDelegate()
             if not new_page_requests:
@@ -58,14 +63,15 @@ def main():
                     _dict = main_dict
                     sleeper.sleep_if_not_empty()
                     Page.do_page(request, _dict, main_page_delegate, new_page_requests)
+                    sleeper.inc_count()
                 break
             else:
                 request, _dict = new_page_requests.pop(0)
                 Page.do_page(request, _dict, main_page_delegate, new_page_requests)
+                sleeper.inc_count()
 
         while HttpFetchProcess.next():
             pass
-        sleeper.inc_count()
         printDebug('<!------------------------------ count = ' + str(sleeper.count_))
         sleeper.check()
 
