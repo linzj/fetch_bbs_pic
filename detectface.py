48e1e06d85a597b966c7240e4b7b6d33d6d95c00
linzj <manjian2006@gmail.com>
2015-11-07 17:28:19 +0800

    add human detection.

--- detectface.py
+++ detectface.py
@@ -12,7 +12,7 @@ USAGE: facedetect.py [--cascade <cascade_fn>] [--nested-cascade <cascade_fn>] [<
 '''
 
 def detect(img, cascade):
-    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv2.CASCADE_SCALE_IMAGE)
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(20, 20), flags = cv2.CASCADE_SCALE_IMAGE)
     if len(rects) == 0:
         return False
     rects[:,2:] += rects[:,:2]
@@ -23,16 +23,18 @@ def draw_rects(img, rects, color):
         cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
 def main():
-    cascade_fn = 'haarcascade_frontalface_default.xml'
-
-    cascade = cv2.CascadeClassifier(cascade_fn)
     if os.path.exists("faces"):
         if not os.path.isdir("faces"):
             shutil.rmtree("faces")
             os.mkdir("faces")
     else:
         os.mkdir("faces")
+    hog()
 
+def fd():
+    cascade_fn = 'lbpcascade_frontalface.xml'
+
+    cascade = cv2.CascadeClassifier(cascade_fn)
     for f in glob.glob('*.jpg'):
         if not os.path.isfile(f):
             break
@@ -48,6 +50,26 @@ def main():
         if detected:
             print("%s contains face" % (f))
             shutil.move(f, os.path.join("faces", f))
+def hog():
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector( cv2.HOGDescriptor_getDefaultPeopleDetector() )
+
+    for f in glob.glob('*.jpg'):
+        try:
+            img = cv2.imread(f)
+            if img is None:
+                print('Failed to load image file:', fn)
+                continue
+        except:
+            print('loading error')
+            continue
+
+        print('detecting %s' % f)
+        found, w = hog.detectMultiScale(img, winStride=(8,8), padding=(32,32), scale=1.05)
+        if len(found) != 0:
+            print("%s contains face" % (f))
+            shutil.move(f, os.path.join("faces", f))
+
 
 if __name__ == '__main__':
     main()
